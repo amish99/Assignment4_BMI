8dabfb1067733b617605fe093374f3012b92823b
amish tangri <amishtangri123@gmail.com>
2020-08-07 23:19:08 -0400

    comment added

--- assignmentBMI4/Program.cs
+++ assignmentBMI4/Program.cs
@@ -6,6 +6,12 @@ using System.Windows.Forms;
 
 namespace assignmentBMI4
 {
+    /*
+ * Name: amish tangri
+ * Student ID: 301108446
+ * Date: August 7, 2020
+ * Program Description: This is a BMI(body mass index) calculator which whill show your bmi after you filled your height and weight.
+ */
     static class Program
     {
         public static BMICalculator bmicalculator;
--- assignmentBMI4/splashscreen.cs
+++ assignmentBMI4/splashscreen.cs
@@ -10,6 +10,12 @@ using System.Windows.Forms;
 
 namespace assignmentBMI4
 {
+    /*
+* Name: amish tangri
+* Student ID: 301108446
+* Date: August 7, 2020
+* Program Description: This is a BMI(body mass index) calculator which whill show your bmi after you filled your height and weight.
+*/
     public partial class SplashForm : Form
     {
         public SplashForm()
